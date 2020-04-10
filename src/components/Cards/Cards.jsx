33531ae6bbefb9ba778878514865cd2a007cc5d1
Stephen Blair <stephenjohnblair@outlook.com>
2020-04-11 00:49:12 +0100

    Added line chart with datasets

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -21,7 +21,7 @@ const Cards = ({ data : { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -31,7 +31,7 @@ const Cards = ({ data : { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
