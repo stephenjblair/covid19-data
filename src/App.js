a94b2b0d7becf314f6b693e945020c9a2aaf8dad
Stephen Blair <stephenjohnblair@outlook.com>
2020-04-11 11:51:32 +0100

    Added bar chart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { fetchData } from './api';
 class App extends React.Component {
     state = {
         data: {},    
+        country: '',
     }
 
 
@@ -14,13 +15,22 @@ class App extends React.Component {
         
         this.setState({ data: fetchedData });
     }
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData, country: country });
+  
+      
+
+    
+    }
     render() {
-        const { data } = this.state;
+        const { data, country  } = this.state;
         return (
                 <div className={styles.container}>
                     <Cards data={data} />
-                    <CountryPicker />
-                    <Chart /> 
+                    <CountryPicker handleCountryChange={this.handleCountryChange} />
+                    <Chart data={data} country={country} /> 
                 </div>
         )
     }
